81f4c99704c960186cd5c2c72b7f05de2d64866c
Minna Imporanta <minnaimporanta@kamk.fi>
2025-05-24 14:10:36 +0300

    3.21

--- osa_3/models/person.js
+++ osa_3/models/person.js
@@ -17,11 +17,10 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minlength: 8,
     required: [true, 'Phone number is required'],
     validate: {
       validator: function (v) {
-        return /^\d{2,3}-\d+$/.test(v)
+        return /^\d{2,3}-\d{5,}$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     }
